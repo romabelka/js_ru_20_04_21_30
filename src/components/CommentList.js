16e0ede281ed3812aae3a1c99be9508ac31136b0
Konstantin Usplenev <usplenievk@undev.ru>
2017-05-14 14:16:25 +0300

    Comment form and validation added

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,35 +1,70 @@
 import React, {Component} from 'react'
-import Comment from './Comment'
-import toggleOpen from '../decorators/toggleOpen'
 import PropTypes from 'prop-types'
+import Comment from './Comment'
+import CommentForm from './CommentForm'
+import toggleOpen from '../decorators/toggleOpen'
 
-function CommentList(props) {
-    const {isOpen, toggleOpen} = props
-    const linkText = isOpen ? 'hide comments' : 'show comments'
+class CommentList extends Component {
 
-    return (
-        <div>
-            <a href="#" onClick={toggleOpen}>{linkText}</a>
-            {getBody(props)}
-        </div>
-    )
-}
+    static propTypes = {
+        isOpen: PropTypes.bool,
+        toggleOpen: PropTypes.func,
+        comments: PropTypes.array
+    }
 
-function getBody(props) {
-    const {comments = [], isOpen} = props
-    if (!isOpen) return null
-    if (!comments.length) return <p>No comments yet</p>
-    return (
-        <ul>
-            {comments.map(comment => <li key={comment.id}><Comment comment={comment}/></li>)}
-        </ul>
-    )
-}
+    constructor(props) {
+        super(props);
 
-CommentList.propTypes = {
-    isOpen: PropTypes.bool,
-    toggleOpen: PropTypes.func,
-    comments: PropTypes.array
+        this.state = {
+            comments: props.comments
+        }
+    }
+
+    componentWillReceiveProps(nextProps) {
+        this.setState({
+            comments: nextProps.comments
+        });
+    }
+
+    getUid() {
+    	return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    		var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+    		return v.toString(16);
+    	});
+    }
+
+    addComment = (comment) => {
+        // тут конечно нужен уникальный id
+        comment.id = this.getUid();
+        this.state.comments.push(comment);
+
+        this.setState({comments: this.state.comments});
+    }
+
+    getBody(comments, isOpen) {
+        if (!isOpen) return null
+        if (!comments.length) return <p>No comments yet</p>
+        return (
+            <div>
+                <CommentForm onSubmit={this.addComment} />
+                <ul>
+                    {comments.map(comment => <li key={comment.id}><Comment comment={comment}/></li>)}
+                </ul>
+            </div>
+        )
+    }
+
+    render() {
+        const {isOpen, toggleOpen} = this.props
+        const linkText = isOpen ? 'hide comments' : 'show comments'
+
+        return (
+            <div>
+                <a href="#" onClick={toggleOpen}>{linkText}</a>
+                {this.getBody(this.state.comments, isOpen)}
+            </div>
+        )
+    }
 }
 
 export default toggleOpen(CommentList)
